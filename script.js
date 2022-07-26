c93d048b87412461f88c611382dba700e1bd881a
Conor Dunne <conordunne@outlook.ie>
2022-07-26 16:32:05 +0900

    Add 'playGame' factory function IIFE to control flow of game

--- script.js
+++ script.js
@@ -1,55 +1,76 @@
-const gameBoard = (()=>{
-    const winningCombos= [[1,2,3], [4,5,6], [7,8,9], [1,4,7],[2,5,8],[3,6,9],[1,5,9],[7,5,3]];
+
+
+const gameBoard = (() => {
+    const winningCombos = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [7, 5, 3]];
     const usedSquares = [];
-    const checkIfWin = () => {
+    const checkIfWin = (array, name) => {
         const winCheck = [];
-        winningCombos.forEach(arr => winCheck.push(arr.every(i => usedSquares.includes(i))));
-        return winCheck.includes(true) ? alert("You win!") : alert("Nice move");
+        winningCombos.forEach(arr => winCheck.push(arr.every(i => array.includes(i))));
+        return winCheck.includes(true) ? alert(`${name} wins!`) : alert("Nice move");
     }
     const placeMarker = (marker, squareNum) => {
-        if (usedSquares.includes(squareNum)) { alert("Please choose another square");};
+        if (usedSquares.includes(squareNum)) { alert("Please choose another square"); };
         document.getElementById(squareNum).textContent = marker;
         usedSquares.push(squareNum);
         console.log(usedSquares);
-        checkIfWin();
     };
 
     return {
-        placeMarker
+        placeMarker,
+        checkIfWin
     };
 
 })();
 
-
-
-
-
 const Player = (name, marker) => {
-    const getName = () => name;
-    const getMarker = () => marker;
+    const playerSquares = [];
+    // const getName = () => name;
+    // const getMarker = () => marker;
     const takeTurn = (marker, square) => {
-        return gameBoard.placeMarker(marker, square);
+        playerSquares.push(square);
+        gameBoard.placeMarker(marker, square);
+        gameBoard.checkIfWin(playerSquares, name);
+
     }
 
-    return {getName, getMarker, takeTurn};
+    return { takeTurn };
 };
 
-const player1 = Player("Conor", "X");
+const player1 = Player("Player X", "X");
+const player2 = Player("Player O", "O");
 
 
-player1.takeTurn("X", 5);
-player1.takeTurn("X", 9);
-player1.takeTurn("X", 1);
+const playGame = (() => {
+    let currentPlayer = player1;
+    let marker = "X";
+    const switchPlayer = () => {
+        if (currentPlayer == player1) {
+            currentPlayer = player2;
+            marker = "O";
+        } else {
+            currentPlayer = player1;
+            marker = "X"
+        };
+    }
+    const playerMove = (num) => {
+        currentPlayer.takeTurn(marker, num);
+        switchPlayer();
+    }
+
+    return { playerMove };
+
+})();
 
 
+const allSquares = Array.from(document.querySelectorAll(".game-square"));
+console.log(allSquares);
 
+allSquares.forEach(sq => sq.addEventListener("click", go))
 
-
-// function test() {
-//     // document.querySelector("#5").textContent = "X";
-//     console.log(document.querySelectorAll(".game-square"));
-//     console.log(document.getElementById("5"));
-// }
+function go() {
+    let num = Number(this.id);
+    playGame.playerMove(num);
+}
 
 
 
